6e3593c677a87ac81c2634d0edcf24305e225c8d
parsnippity <briannesiemsen@gmail.com>
2023-12-07 13:03:59 -0700

    added title

--- src/Components/Quote.jsx
+++ src/Components/Quote.jsx
@@ -1,6 +1,7 @@
 const Quote = ({quote}) => {
   return (
     <div id="theInfo">
+        <h1 id="title">Random Quote Generator</h1>
         <h1 id="quote">"{quote.data.content}"</h1>
         <h2 id="author">- {quote.data.author}</h2>
     </div>
